4d83063c3df4155bcb33f0c3e7a37de96d690c5e
Y CRONJE <yakocronje@gmail.com>
2022-09-23 08:31:23 +0200

    Interfeace Generic Repository Code

--- ConnectedOffice/DeviceManagement_WebApp/Repository/IGenericRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/IGenericRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace DeviceManagement_WebApp.Repository
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System;
+
+namespace DeviceManagement_WebApp.Repository
 {
-    public class IGenericRepository
+    public interface IGenericRepository<C> where C : class
     {
+        C GetById(Guid id);
+        IEnumerable<C> GetAll();
+        IEnumerable<C> Find(Expression<Func<C,bool>> expression);
+        bool Exists(Expression<Func<C,bool>> expression);
+        void add(C entity);
+        void addrange(IEnumerable<C> entities);
+        void Update(C entity);
+        void RemoveRange(IEnumerable<C> entities);
+        void remove(C entity);
+        void Save();
+        
     }
 }
