0410fcad43e1b77f6ef56879df30c4da45327189
Y CRONJE <yakocronje@gmail.com>
2022-09-26 15:12:43 +0200

    device controller done

--- ConnectedOffice/DeviceManagement_WebApp/Repository/IDeviceRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/IDeviceRepository.cs
@@ -1,8 +1,24 @@
 ﻿using DeviceManagement_WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace DeviceManagement_WebApp.Repository
 {
     public interface IDeviceRepository : IGenericRepository<Device>
     {
+        public Device GetMostRecentDevice();
+
+        public IEnumerable<Device> GetAllDevices();
+
+        public Device GetDeviceDetails(Guid id);
+
+        public IEnumerable<Zone> GetZone();
+
+        public IEnumerable<Category> GetCategories();
+
+        public IEnumerable<Device> SortDevices(Expression<Func<Device, String>> expression);
+
+        public Device FindDevice(Expression<Func<Device, bool>> expression);  
     }
 }
